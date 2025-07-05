ff672b7378623e2e01294e77941fd13a3565e0ee
Alisabeth <pisarevalisabeth@gmail.com>
2025-07-05 18:46:15 +0300

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 100; //счет клиента, начальные данные
-        int b; //бонусы
-        int c = 1250; //сумма пополнения
-        int d; //новый размер счета после пополнения
-        d = a + c;
-
-        if (c >= a)
-        b = c / a;
-                        else b = 0;
-
-        System.out.println("состояние счета: " + d);
-    System.out.println("начислено бонусов: " + b);
+        int inputData = 100; //счет клиента, начальные данные
+        int bonus; //бонусы
+        int amount = 1250; //сумма пополнения
+        int sum; //новый размер счета после пополнения
+        sum = inputData + amount;
+        {
+            if (amount >= inputData)
+                bonus = amount / inputData;
+            else bonus = 0;
+        }
+        System.out.println("состояние счета: " + sum);
+        System.out.println("начислено бонусов: " + bonus);
 
     }
 }
