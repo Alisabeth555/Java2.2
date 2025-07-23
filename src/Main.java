2f9619e017f85c0997dfbab2a16fb0cb06fa3eb6
Alisabeth <pisarevalisabeth@gmail.com>
2025-07-23 18:18:35 +0300

    modified
    modified:   src/Main.java
    recording @r

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         int amount = 1250; //сумма пополнения
         int sum; //новый размер счета после пополнения
         sum = inputData + amount;
-        {
-            if (amount >= inputData)
-                bonus = amount / inputData;
-            else bonus = 0;
+        if (amount >= inputData) {
+            bonus = amount / inputData;
+        } else {
+            bonus = 0;
         }
         System.out.println("состояние счета: " + sum);
         System.out.println("начислено бонусов: " + bonus);
