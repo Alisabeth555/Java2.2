a33c6f91896d07b6995fa6c08123981127407787
Alisabeth <pisarevalisabeth@gmail.com>
2025-07-25 19:10:15 +0300

    bonusAdded

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int inputData = 500; //счет клиента, начальные данные
+        int inputData = 100; //счет клиента, начальные данные
         int bonus; //бонусы
-        int amount = 1325; //сумма пополнения
+        int amount = 1100; //сумма пополнения
         int sum; //новый размер счета после пополнения
-        sum = inputData + amount;
+
         if (amount >= 1000) {
             bonus = amount / 100;
         } else {
             bonus = 0;
         }
+        sum = inputData + amount + bonus;
         System.out.println("состояние счета: " + sum);
         System.out.println("начислено бонусов: " + bonus);
 
